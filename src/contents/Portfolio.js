ae3b58b8faa1d6d7c425cf861ca49010a083ad80
Owais Sultan <owaissultan958@gmail.com>
2021-02-05 07:06:03 +0500

    new prod

--- src/contents/Portfolio.js
+++ src/contents/Portfolio.js
@@ -10,10 +10,10 @@ class Portfolio extends Component {
             <div className="condiv">
             <h1 className="subtopic">My Work</h1>
             <div className="hr_line line2 glass"><div className="inner2 inner3"></div></div>
-            <div class="port_out port1 glass"><a href="https://github.com/owaissultan3000/netflix-clone"><img src={project1} className="po1"></img></a></div>
-            <div class="port_out port2 glass"><a href="https://github.com/owaissultan3000/Expense-Tracker-App"><img src={project2} className="po1"></img></a></div>
-            <div class="port_out port3 glass"><a href="https://github.com/owaissultan3000/facebook-clone"><img src={project3} className="po1"></img></a></div>
-            <div class="port_out port4 glass"><a href="https://github.com/owaissultan3000/ALGO"><img src={project4} className="po1"></img></a></div>
+            <div class="port_out port1 glass"><a href="https://github.com/owaissultan3000/netflix-clone"><img src={project1} alt ="" className="po1"></img></a></div>
+            <div class="port_out port2 glass"><a href="https://github.com/owaissultan3000/Expense-Tracker-App"><img src={project2} alt ="" className="po1"></img></a></div>
+            <div class="port_out port3 glass"><a href="https://github.com/owaissultan3000/facebook-clone"><img src={project3} alt ="" className="po1"></img></a></div>
+            <div class="port_out port4 glass"><a href="https://github.com/owaissultan3000/ALGO"><img src={project4} alt ="" className="po1"></img></a></div>
             </div>
             )
         }
